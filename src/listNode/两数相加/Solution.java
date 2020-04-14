ffbe0d548691a8193128ed9e8517e88a0a1712cc
aestheticJL <m258789@126.com>
2020-04-14 14:47:39 +0800

    两数相加2

---
+++ src/listNode/两数相加/Solution.java
@@ -0,0 +1,65 @@
+/*
+445. 两数相加 II
+给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+
+你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+进阶：
+
+如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+
+示例：
+
+输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 8 -> 0 -> 7
+### 解题思路
+    将链表压入栈中再逐个取值赋值给结果链表
+    执行用时 :5 ms, 在所有 Java 提交中击败了59.59%的用户
+    内存消耗 :40.5 MB, 在所有 Java 提交中击败了95.83%的用户
+ */
+package listNode.两数相加;
+
+import java.util.Stack;
+
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        int carry = 0;
+        ListNode result = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int sum = carry;
+            sum+=stack1.isEmpty()?0:stack1.pop();
+            sum+=stack2.isEmpty()?0:stack2.pop();
+            ListNode newNode = new ListNode(sum % 10);
+            newNode.next = result;
+            result = newNode;
+            carry = sum / 10;
+        }
+        return result;
+    }
+}
