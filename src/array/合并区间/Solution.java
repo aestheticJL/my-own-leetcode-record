a5faa314d820bea8211fde03193f7144d4fd1deb
aestheticJL <m258789@126.com>
2020-04-17 14:50:00 +0800

    跳跃游戏

---
+++ src/array/合并区间/Solution.java
@@ -0,0 +1,20 @@
+package array.合并区间;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        int[][] result = new int[intervals.length][2];
+        int temp = -1;
+        for (int[] interval : intervals) {
+            if (temp == -1 || interval[0] > result[temp][1]) {
+                result[++temp] = interval;
+            } else {
+                result[temp][1] = Math.max(result[temp][1], interval[1]);
+            }
+        }
+        return Arrays.copyOf(result, temp + 1);
+    }
+}
---
+++ src/array/跳跃游戏/Solution.java
@@ -0,0 +1,41 @@
+ /*
+ 55. 跳跃游戏
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个位置。
+
+示例 1:
+
+输入: [2,3,1,1,4]
+输出: true
+解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+示例 2:
+
+输入: [3,2,1,0,4]
+输出: false
+解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+###解题思路
+使用动态规化即可，每一个点都判断所能达到的最大值
+
+执行用时 :3 ms, 在所有 Java 提交中击败了36.96%的用户
+内存消耗 :41.9 MB, 在所有 Java 提交中击败了12.50%的用户
+  */
+package array.跳跃游戏;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i;j<nums.length&&j<=dp[i];j++){
+                dp[i] = Math.max(dp[i], nums[j]+j);
+                if (dp[i]>=nums.length-1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
