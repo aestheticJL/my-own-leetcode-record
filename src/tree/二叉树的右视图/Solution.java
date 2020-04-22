c383c06aed60e49ff679298e965e8db9c1dfa29d
aestheticJL <m258789@126.com>
2020-04-22 18:45:06 +0900

    二叉树的右视图

---
+++ src/tree/二叉树的右视图/Solution.java
@@ -0,0 +1,66 @@
+/*
+199. 二叉树的右视图
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+
+示例:
+
+输入: [1,2,3,null,5,null,4]
+输出: [1, 3, 4]
+解释:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+###解题思路
+  类似树的先序遍历，不过先访问右节点
+执行用时 :1 ms, 在所有 Java 提交中击败了97.36%的用户
+内存消耗 :38.2 MB, 在所有 Java 提交中击败了5.00%的用户
+ */
+package tree.二叉树的右视图;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size != 0) {
+                TreeNode poll = queue.poll();
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+                if (size == 1) {
+                    res.add(poll.val);
+                }
+                size--;
+            }
+        }
+        return res;
+    }
+}
