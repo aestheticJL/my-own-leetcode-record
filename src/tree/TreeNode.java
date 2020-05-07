04d734249176c755e3ae6c4a3830f47ace284698
aestheticJL <m258789@126.com>
2020-05-07 14:08:13 +0800

    相同的树&&另一个树的子树

--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -1,11 +1,20 @@
 package tree;
 
 public class TreeNode {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-        public TreeNode(int x) {
-            val = x;
-        }
+    TreeNode(int x) {
+        val = x;
     }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    TreeNode() {
+    }
+}
