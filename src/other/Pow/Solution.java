0692587151f4d70b97478e04bf33ae2aa3846e32
aestheticJL <m258789@126.com>
2020-05-11 16:42:09 +0800

    pow

---
+++ src/other/Pow/Solution.java
@@ -0,0 +1,40 @@
+/*
+Pow(x, n)
+实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+
+示例 1:
+
+输入: 2.00000, 10
+输出: 1024.00000
+示例 2:
+
+输入: 2.10000, 3
+输出: 9.26100
+示例 3:
+
+输入: 2.00000, -2
+输出: 0.25000
+解释: 2-2 = 1/22 = 1/4 = 0.25
+说明:
+
+-100.0 < x < 100.0
+n 是 32 位有符号整数，其数值范围是 [Integer.MIN_VALUE, Integer.MAX_VALUE] 。
+###解题思路
+幂运算
+当n=Integer.MIN_VALUE 栈溢出
+ */
+package other.Pow;
+
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n < 0) return 1 / myPow(x, -n);
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        } else {
+            return myPow(x, n - 1) * x;
+        }
+
+    }
+}
