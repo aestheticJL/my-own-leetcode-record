d78be880d1fc0a21b4eced8f3d327cf7e8711907
aestheticJL <m258789@126.com>
2020-07-12 18:57:12 +0800

    双线程交叉打印奇偶数&地下城游戏

---
+++ src/multithreading/双线程交叉打印奇偶数/Solution.java
@@ -0,0 +1,31 @@
+package multithreading.双线程交叉打印奇偶数;
+
+public class Solution implements Runnable{
+    static int sum = 1;
+
+    @Override
+    public void run(){
+        while (sum <= 100) {
+            synchronized (Solution.class) {
+                System.out.println(Thread.currentThread().getName() + "打印了：" + sum++);
+                Solution.class.notify();
+                if (sum <= 100) {
+                    try {
+                        Solution.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 2, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+//        threadPoolExecutor.submit(new Solution());
+        new Thread(new Solution(), "奇数线程池").start();
+        Thread.sleep(1);
+        new Thread(new Solution(), "偶数线程池").start();
+
+    }
+}
