d6dfbb8af9834cbb9861327a13f5df66046f9a57
aestheticJL <m258789@126.com>
2020-08-23 00:57:42 +0800

    零钱兑换1&2

---
+++ src/dp/零钱兑换/Solution.java
@@ -0,0 +1,49 @@
+/*
+322. 零钱兑换
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+
+
+
+示例 1:
+
+输入: coins = [1, 2, 5], amount = 11
+输出: 3
+解释: 11 = 5 + 5 + 1
+示例 2:
+
+输入: coins = [2], amount = 3
+输出: -1
+
+
+说明:
+你可以认为每种硬币的数量是无限的。
+###解题思路
+dp
+执行用时：4 ms, 在所有 Java 提交中击败了60.16%的用户
+内存消耗：37.1 MB, 在所有 Java 提交中击败了78.53%的用户
+ */
+package dp.零钱兑换;
+
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for (int c : coins) {
+            if (c <= amount)
+                dp[c] = 1;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int c : coins) {
+                if (i > c && dp[i - c] > 0)
+                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+            }
+            if (dp[i] == Integer.MAX_VALUE) dp[i] = -1;
+        }
+        return dp[amount];
+    }
+}
